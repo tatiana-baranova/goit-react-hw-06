85953a7067bbc6bf760a69e747d8f1b4a6247ae8
tetiana_baranova <syniovych.tanya@gmail.com>
2025-03-04 20:35:17 +0200

    searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import SearchBox from './component/SearchBar/SearchBar'
 
 function App() {
 
   return (
     <>
+      <div className="container">
+        <h1 className="title">Phonebook</h1>
+        <SearchBox/>
+      </div>
     </>
   )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from "redux-persist";
+import contactsReducer from './contactsSlice'
+import filtersReducer from './filtersSlice'
+
+const persistConfigContacts = {
+    key: 'contacts',
+    storage,
+    version: 1,
+    whitelist: ["items"],
+};
+
+const rootReducer = combineReducers({
+    contacts: persistReducer(persistConfigContacts, contactsReducer),
+    filters: filtersReducer,
+});
 
 
 
 export const store = configureStore({
-    // reduce: {
-    //     contacts: persistedReducerContacts,
-    //     filters: filtersReducer,
-    // },
+    reducer: rootReducer,
 });
+
+export const persist = persistStore(store);
