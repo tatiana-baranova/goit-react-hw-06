3a77a7a261cf7135b4a02d14fcc8d2c6b6a88355
tetiana_baranova <syniovych.tanya@gmail.com>
2025-03-04 14:39:24 +0200

    contactsSlice

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import "modern-normalize";
 import App from './App.jsx'
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </StrictMode>,
 )
