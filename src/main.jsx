ae99a172e404cef786e427e81c595411ad3883e1
tetiana_baranova <syniovych.tanya@gmail.com>
2025-03-04 23:29:46 +0200

    finish

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import "modern-normalize";
 import App from './App.jsx'
 import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store.js';
+import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
--- src/redux/store.js
+++ src/redux/store.js
@@ -28,4 +28,5 @@ export const store = configureStore({
     }),
 });
 
+
 export const persistor = persistStore(store);
